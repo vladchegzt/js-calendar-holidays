ad53f42bc2823121849acad5d810e4211738a24c
verstunio <verstunio@syneforge.com>
2023-07-28 14:16:36 +0300

    homework pre version

--- app/js/index.js
+++ app/js/index.js
@@ -1,74 +1,102 @@
 window.addEventListener("load", function() {
-    /**
-     * tab 1 functional
-    **/
-
-    //dom elements
-    const inputsDates = document.querySelectorAll('.date-range-item');
-    const inputStart = document.getElementById('startDate');
-    const inputEnd = document.getElementById('endDate');
-    const resultBtn = document.getElementById('result-btn');
-    const resultElement = document.getElementById('result-difference');
-    let resultValue = 0;
-
-    // validation listeners
-    const disablingResultBtn = () => {
-        resultBtn.disabled = inputStart.value.trim() === '' || inputEnd.value.trim() === '';
-    };
-
-    inputsDates.forEach(item => {
-        item.addEventListener('change', (event)=> {
-            // console.log(el)
-            if(item === inputStart) {
-                inputEnd.disabled = inputStart.value.trim() === '' ? true : false;
-            }
-            disablingResultBtn()
+    // 1. Напишіть функцію addThemAll
+    // Вона буде знаходити суму усіх своїх аргументів незалежно від їх кількості
+       
+    // вирішив працювати і з string і з float.
+    function addThemAllFloat (...nums) {
+        let arr = nums.map((item, index) => {
+            return +item;
         });
-    });
+        return arr.reduce((accumulator, item) => {return accumulator + item}, 0);
+    }   
+    console.group();
+    console.log(addThemAllFloat(1,' 8.22',-3));
+    console.log(addThemAllFloat(1,2,13));
+    console.log(addThemAllFloat(1,2,'13'));
+    console.groupEnd('Task 1');
 
-    const countFinalResult = (start, end, dimension = 'days') => {
-        start = new Date(start.value).getTime();
-        end = new Date(end.value).getTime();
-        dimension = dimension.toLowerCase();
-        let result;
+    // 2. Задача на використання замикання. 
+    console.group();
+    console.log(multiply(2)(-2));
+    console.log(multiply('4')('3'));
+    console.log(multiply(4)(3));
+    console.groupEnd();
 
-        const diffMiliseconds = Math.floor(end - start);
-
-        // convert to desirable dimension
-        const dimensionActions = {
-            days: {
-                text: 'дні(-в)',
-                action: () => Math.floor(diffMiliseconds / (1000 * 60 * 60 * 24)),
-            },
-            hours: {
-                text: 'годин',
-                action: () => Math.floor(diffMiliseconds / (1000 * 60 * 60)),
-            },
-            minutes: {
-                text: 'хвилин',
-                action: () => Math.floor(diffMiliseconds / (1000 * 60)),
-            },
-            seconds: {
-                text: 'секунд',
-                action: () => Math.floor(diffMiliseconds / 1000 * 60),
-            },
-        }
-
-        // check if provided @dimension exist in obj
-        if (Object.keys(dimensionActions).includes(dimension)) {
-            result = `Різниця – ${dimensionActions[dimension].action()} ${dimensionActions[dimension].text}`;
-        } else {
-            result = `Різниця – ${dimensionActions.days.action()} ${dimensionActions.days.text}`;
-        }
-        // return result;
-        resultElement.textContent = result;
+    function multiply(a) {
+        return function(b) {
+          return +a * +b;
+        };
     }
 
-    resultBtn.addEventListener('click', () => {
-        countFinalResult(inputStart, inputEnd, 'days');
-    })
+       // 4. Напишіть функцію яка відфільтрує масив унікальних значень
+       const userNames = ['Петро', 'Емма', 'Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена', 'Емма'];
 
+       function filterUnique(array) {
+           const uniqNames = new Set();
+           for (let item of array) {
+               uniqNames.add(item)
+           }
+           return uniqNames
+       }
+       console.group();
+       console.log(filterUnique(userNames));
+       console.groupEnd();
+
+    // /3. Напишіть функцію яка буде використовуватись для сортування масиву фільмів 
+    console.group();
+    const movies = [
+        {
+            movieName: 'The Thing',
+            releaseYear: 1982,
+            directedBy: 'Carpenter',
+            runningTimeInMinutes: 109,
+        },
+        {
+            movieName: 'Aliens',
+            releaseYear: 1986,
+            directedBy: 'Cameron',
+            runningTimeInMinutes: 137,
+        },
+        {
+            movieName: 'Men in Black',
+            releaseYear: 1997,
+            directedBy: 'Sonnenfeld',
+            runningTimeInMinutes: 98,
+        },
+        {
+            movieName: 'Predator',
+            releaseYear: 1987,
+            directedBy: 'McTiernan',
+            runningTimeInMinutes: 107,
+        },
+    ];
     
-    console.log();
-    disablingResultBtn();
+    function byProperty(property, direction) {
+       return function (a, b) {
+        let iteration;
+        if(property === 'movieName') {
+            iteration = a[property].toLowerCase() - b[property].toLowerCase()
+        }
+        else {
+            iteration = a[property] - b[property]
+        }
+
+        if(direction === '>') {
+            return iteration
+        } else {
+            return -iteration;
+        }
+       }
+    }
+
+    console.log(movies.sort(byProperty('releaseYear', '<'))); 
+    // виведе масив фільмів посортованих по року випуску, від старішого до новішого
+    console.log(movies.sort(byProperty('runningTimeInMinutes', '<'))); 
+    // // виведе масив фільмів посортованих по їх тривалості, від найдовшого до найкоротшого
+    console.log(movies.sort(byProperty('movieName', '>'))); 
+    // виведе масив фільмів посортованих по назві, в алфавітному порядку
+    console.groupEnd();
+
 });
+
+
