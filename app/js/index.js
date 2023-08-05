aa3c3f8822aaf12260184de0c39d5daa33a68530
verstunio <vladchegzt@gmail.com>
2023-08-05 23:27:58 +0300

    first review HM 4

--- app/js/index.js
+++ app/js/index.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function() {
     // Вона виводить в консоль число кожну секунду, 
     // починаючи з delay (ціле число) і в кінці замість 0 виведе 'BOOM!'
     
-    // detonatorTimer(3);
+    detonatorTimer(3);
  
     function detonatorTimer(delay) {
         let counter = delay;
@@ -37,5 +37,58 @@ window.addEventListener("load", function() {
         }
 
     }
+
+    //3
+    let character = {
+        name: "Barbie",
+        age: 30,
+        role: "Developer", 
+        location: "New York",
+        greet() {
+            console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+        },
+        getLocation () { 
+            console.log(`My location is ${this.location}.`);
+        }
+    }
+
+    let securedSelfGreet = character.greet.bind(character);
+    let securedSelfChangeLocation = character.getLocation.bind(character);
+    
+    // character.greet = function () { //не перезапише securedSelfGreet
+    //     console.log(111) 
+    // }
+    
+    setTimeout(securedSelfGreet, 1000); 
+    setTimeout(securedSelfChangeLocation, 2000); 
+
+
+    // 5. Напишіть функцію-декоратор яка вповільнює виконання довільної 
+    // функції на вказану кількість секунд.
+
+    // function someFunction // тут напишіть довільну функцію яка робить 
+    // якусь роботу зі своїми аргументами та виводить результат в консоль
+    function someFunction (x, y) {
+        return console.log(`Result someFunction is ${x + y}`)
+    }
+
+    function slower(func, seconds) {
+        return function (...args) {
+            console.log(`Chill out, you will get your result in ${seconds} seconds`);
+            setTimeout(() => {
+                func(...args);
+            }, seconds * 1000);
+        }
+    }
+    
+    // обгортаєте свою довільну функцію 'someFunction' в декоратор і задає значення вповільнення
+    let slowedSomeFunction = slower(someFunction, 5); 
+    
+    slowedSomeFunction(2, 6)
+    
+    // виведе в консоль "Chill out, you will get you result in 5 seconds
+    //...через 5 секунд виведе результат роботи 'someFunction'
+
+
 
 });
